2619a4b34ed38c1c12cc07a53e99a382e42bab09
Jihoon LEE <82790390+hnc-jihoon@users.noreply.github.com>
2021-08-10 10:12:17 +0900

    Add cat markers on the sample map
    - show a cat icon, temporarily

--- pages/map-sample.tsx
+++ pages/map-sample.tsx
@@ -1,41 +1,77 @@
 import React, { useEffect } from "react";
 import MapComponent from "../components/mapComponent";
+import getCatPositions from "../channel/catPositions";
 
+// TODO: fix types
+type Map = {};
+type Size = {};
+type Image = {};
+
+const createMarkerImage = (src: string, size: Size, options?: any) => {
+  const { kakao } = window as any;
+  return new kakao.maps.MarkerImage(src, size, options);
+}
+
+// 임시 마커 생성 - 고양이 아이콘
+// TODO: use image thumbnail
+const createMarkers = (map: Map) => {
+  const { kakao } = window as any;
+  const imgSrc = 'https://icon-library.com/images/cat-face-icon/cat-face-icon-4.jpg';
+  const imgSize = new kakao.maps.Size(60, 54);
+
+  const Marker = kakao.maps.Marker;
+  const LatLng = kakao.maps.LatLng;
+
+  const markers = getCatPositions().map(pos => {
+    return new Marker({
+      position: new LatLng(pos.lat, pos.lng),
+      image: createMarkerImage(imgSrc, imgSize),
+      map,
+    })
+  });
+
+  return markers;
+};
+
+
+// source ref)
+// https://flamingotiger.github.io/frontend/react/react-kakao-map/
+// https://gingerkang.tistory.com/65
 const Map: React.FC = () => {
   const kakaoMap = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!(window as any).kakao) {
+    const { kakao } = window as any;
+
+    if (!kakao) {
       console.error('kakao map api not loaded!');
       return;
     }
 
-    (window as any).kakao.maps.load(() => {
+    kakao.maps.load(() => {
       if (!kakaoMap || !kakaoMap.current) {
         console.error('map element is not ready!');
         return;
       }
 
-      const x = 126.570667;
-      const y = 33.450701;
-      const coords = new (window as any).kakao.maps.LatLng(y, x); // 지도의 중심좌표
+      // init position: 강남역 사거리
+      const x = 127.02770621963765;
+      const y = 37.498004414546934;
+      const coords = new kakao.maps.LatLng(y, x); // 지도의 중심좌표
       const options = {
         center: coords,
         level: 2,
       };
-      const map = new (window as any).kakao.maps.Map(kakaoMap.current, options);
-      const marker = new (window as any).kakao.maps.Marker({
-        position: coords,
-        map,
-      });
+      const map = new kakao.maps.Map(kakaoMap.current, options);
+
+      createMarkers(map);
+
       // 맵의 중앙으로 이동
       map.relayout();
       map.setCenter(coords);
-      // 마커를 중앙으로 이동
-      marker.setPosition(coords);
 
       // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-      const mapTypeControl = new (window as any).kakao.maps.MapTypeControl();
+      const mapTypeControl = new kakao.maps.MapTypeControl();
 
       // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
       // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
@@ -44,10 +80,10 @@ const Map: React.FC = () => {
         (window as any).kakao.maps.ControlPosition.TOPRIGHT
       );
       // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-      const zoomControl = new (window as any).kakao.maps.ZoomControl();
+      const zoomControl = new kakao.maps.ZoomControl();
       map.addControl(
         zoomControl,
-        (window as any).kakao.maps.ControlPosition.RIGHT
+        kakao.maps.ControlPosition.RIGHT
       );
     })
   }, [kakaoMap]);
