4a08f7691b163d539e6ab46f213e2ba1a7aeb998
김건우 <keonwoo.kim@hancom.com>
2021-08-22 17:41:10 +0900

    add cognito userpool

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  switch (phase) {
+    case PHASE_DEVELOPMENT_SERVER:
+      return {
+        // environment varibales for local development
+        env: {
+          IDP_DOMAIN: "my-neighbor-cat1.auth.ap-northeast-2.amazoncognito.com",
+          USER_POOL_ID: "ap-northeast-2_uB9TxvgQb",
+          USER_POOL_CLIENT_ID: "7q3tg8thh700rh4rrmpjqi4abe",
+          REDIRECT_SIGN_IN: "http://localhost:3000/token",
+          REDIRECT_SIGN_OUT: "http://localhost:3000/",
+          AUTH_COOKIE_DOMAIN: "localhost",
+        },
+      };
+    default:
+      return {
+        // environment varibales for production
+        env: {
+          IDP_DOMAIN: "my-neighbor-cat1.auth.ap-northeast-2.amazoncognito.com",
+          USER_POOL_ID: "ap-northeast-2_uB9TxvgQb",
+          USER_POOL_CLIENT_ID: "7q3tg8thh700rh4rrmpjqi4abe",
+          REDIRECT_SIGN_IN: "http://localhost:3000/token",
+          REDIRECT_SIGN_OUT: "http://localhost:3000/",
+          AUTH_COOKIE_DOMAIN: "localhost",
+        },
+      };
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,28 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React from "react";
+import { GetServerSideProps } from "next";
+import {
+  AuthTokens,
+  useAuth,
+  useAuthFunctions,
+  getServerSideAuth,
+} from "../auth";
 
-export default function Home() {
+
+export const getServerSideProps: GetServerSideProps<{
+  initialAuth: AuthTokens;
+}> = async (context) => {
+  const initialAuth = getServerSideAuth(context.req);
+
+  return { props: { initialAuth } };
+};
+
+export default function Home(props: { initialAuth: AuthTokens }) {
+  const auth = useAuth(props.initialAuth);
+  const { login, logout } = useAuthFunctions();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,6 +41,20 @@ export default function Home() {
         <p className={styles.description}>
           (여기 대충 앱 소개)
         </p>
+
+        <React.Fragment>
+          {auth ? (
+            <button type="button" onClick={() => logout()}>
+              sign out
+            </button>
+          ) : (
+            <React.Fragment>
+              <button type="button" onClick={() => login()}>
+                sign in
+              </button>
+            </React.Fragment>
+          )}
+        </React.Fragment>
 
         <div className={styles.grid}>
           {/*
