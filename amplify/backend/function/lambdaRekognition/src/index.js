fd84175c58cff5562b9d11478e51196aa4828e80
Jihoon LEE <82790390+hnc-jihoon@users.noreply.github.com>
2021-09-06 16:48:01 +0900

    Modify lambdaRekognition to write the result to DynamoDB
    - new column in DynamoDB: 'is_cat' as true/false
    - TODO: show the result in frontend

--- amplify/backend/function/lambdaRekognition/src/index.js
+++ amplify/backend/function/lambdaRekognition/src/index.js
@@ -1,8 +1,17 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_CATSDB2_ARN
+	STORAGE_CATSDB2_NAME
+	STORAGE_CATSDB2_STREAMARN
+Amplify Params - DO NOT EDIT */
+
 'use strict';
 
 const AWS = require('aws-sdk');
 const rekognition = new AWS.Rekognition();
 const s3 = new AWS.S3();
+const dbClient = new AWS.DynamoDB.DocumentClient();
 
 function detectLabels(bucket, key) {
     const params = {
@@ -42,6 +51,35 @@ function detectLabels(bucket, key) {
      return s3.deleteObject(params).promise(); 
   }
 
+function scanDb(filename) {
+  const scanParams = {
+    TableName : process.env.STORAGE_CATSDB2_NAME,
+    FilterExpression: "contains(photo_url, :f)",
+    ExpressionAttributeValues: {
+      ":f": filename,
+    },
+  };
+
+  return dbClient.scan(scanParams).promise();
+}  
+
+function updateDb(catId, dataType, isACat) {
+  const updateParams = {
+    TableName: process.env.STORAGE_CATSDB2_NAME,
+    Key: {
+      cat_id: catId,
+      data_type: dataType,
+    },
+    UpdateExpression: "set is_cat = :is_cat",
+    ExpressionAttributeValues: {
+        ":is_cat": isACat,
+    },
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  return dbClient.update(updateParams).promise();
+}
+
 exports.handler = async (event) => {
   // handle SNS topic event: 'cat-photo-uploaded'
   const snsMsgObj = JSON.parse(event.Records[0].Sns.Message);
@@ -52,19 +90,36 @@ exports.handler = async (event) => {
 
   console.log(`A file named ${key} was put in a bucket ${bucket}`);
 
+  let isACat;
+
   // Detect the labels
   return detectLabels(bucket, key).then(labels => {
     console.log(labels);
-    const isACat = checkIsACat(labels);
+    isACat = checkIsACat(labels);
 
     console.log(isACat);
-    if (!isACat) {
-      return removeImage(bucket, key).then(() => {
-        console.log('The image was not a cat and was removed');
-        return;
-      });
-    }
-  }).catch(error => {
+    // if (!isACat) {
+    //   return removeImage(bucket, key).then(() => {
+    //     console.log('The image was not a cat and was removed');
+    //     return;
+    //   });
+    // }
+
+    console.log('call scanDb');
+    return scanDb(key);
+  })
+  .then(data => {
+    console.assert(data.Items.length === 1);
+    const item = data.Items[0];
+
+    console.log('call updateDb: cat_id ' + item.cat_id);
+    return updateDb(item.cat_id, item.data_type, isACat);
+  })
+  .then(data => {
+    console.log('done: ' + JSON.stringify(data));
+  })
+  .catch(error => {
+    console.error('error: ' + error);
     return error;
   })
 
