ca7b5b3296af267a68626d6fd8cab6d09e7de4c4
Jihoon LEE <82790390+hnc-jihoon@users.noreply.github.com>
2021-12-07 17:48:14 +0900

    Restore lambdaForThumbnail with Amplify CLI
    - ref) https://github.com/hnc-jihoon/my-neighbor-cats/commit/3d7d2c0f9266b5604a608ccb627b5490dfecd33a
    - the original code was written by @hnc-kaisin
    - cmd: `amplify add function`
      - the exact cmd history was lost (sorry)
      - (s3 catsbucket access, nodejs, hello world example, S3 CRUD, etc.)
    - added a dependency in the Lambda source dir: `yarn add sharp`
    - and `amplify push`

---
+++ amplify/backend/function/lambdaForThumbnail/src/index.js
@@ -0,0 +1,80 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_CATSBUCKET_BUCKETNAME
+Amplify Params - DO NOT EDIT */
+
+const AWS = require('aws-sdk');
+const util = require('util');
+const s3 = new AWS.S3({ signatureVersion: 'v4' });
+const sharp = require('sharp');
+
+exports.handler = async (event) => {
+    // handle SNS topic event: 'cat-photo-uploaded'
+    const snsMsgObj = JSON.parse(event.Records[0].Sns.Message);
+    const s3Record = snsMsgObj.Records[0].s3;
+
+    const srcBucket = s3Record.bucket.name;
+    const srcKey = decodeURIComponent(s3Record.object.key.replace(/\+/g, ' '));
+
+    const thumbBucket = srcBucket + '-resized';
+    const thumbKey = 'resized-' + srcKey;
+
+    const typeMatch = srcKey.match(/\.([^.]*)$/);
+    if (!typeMatch) {
+        console.log('Could not determine the image type.');
+        return;
+    }
+    const imageType = typeMatch[1].toLowerCase();
+    if (imageType != 'jpeg' && imageType != 'jpg' && imageType != 'png') {
+        console.log(`Unsupported image type: ${imageType}`);
+        return;
+    }
+
+    try {
+        const params = {
+            Bucket: srcBucket,
+            Key: srcKey,
+        };
+        var origimage = await s3.getObject(params).promise();
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    const size = 200;
+
+    try {
+        const info = await sharp(origimage.Body).metadata();
+        const left = info.width > size ? parseInt(info.width / 4) : 0;
+        const top = info.height > size ? parseInt(info.height / 4) : 0;
+        const width = info.width > size ? parseInt(info.width / 2) : size;
+        const height = info.height > size ? parseInt(info.height / 2) : size;
+
+        var buffer = await sharp(origimage.Body).extract({left:left, top:top, width:width, height:height}).toBuffer();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+
+    try {
+        const destparams = {
+            Bucket: thumbBucket,
+            Key: thumbKey,
+            Body: buffer,
+            ContentType: "image",
+            ACL: 'public-read'
+        };
+
+        const putResult = await s3.putObject(destparams).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    console.log('Successfully resized ' + srcBucket + '/' + srcKey +
+        ' and uploaded to ' + thumbBucket + '/' + thumbKey);
+};
