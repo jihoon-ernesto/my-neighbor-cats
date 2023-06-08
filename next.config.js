bb241692245ba56c45bc4653c59e7920181b7216
Ernie (Jihoon LEE) <82790390+hnc-jihoon@users.noreply.github.com>
2023-06-09 00:20:19 +0900

    Fix S3 bucket name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   images: {
     domains: [
-      'catsbucket215343-main.s3.ap-northeast-2.amazonaws.com',
+      'catsbucket113003-mainback.s3.ap-northeast-2.amazonaws.com',
       'catsbucket150419-devjihoon.s3-ap-northeast-2.amazonaws.com',
     ],
   },
